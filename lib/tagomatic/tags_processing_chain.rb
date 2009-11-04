a1e94cc27405d05045c51ffc86968503b1d5e76d
Daniel Lukic <daniel.lukic@berlinfactor.com>
2009-11-04 17:13:35 +0100

    Fixed TagsProcessingChain: The chain is now recreated for every run. This way local options will have an effect again. Could be optimizied, I guess.

--- lib/tagomatic/tags_processing_chain.rb
+++ lib/tagomatic/tags_processing_chain.rb
@@ -6,24 +6,25 @@ module Tagomatic
       @options = options
       @tags_processor_factory = tags_processor_factory
       @logger = logger
-      create_processors
     end
 
     def process!(tags_hash)
       return if tags_hash.nil? or tags_hash.empty?
-      @processors.each do |processor|
+      chain = create_processor_chain
+      chain.each do |processor|
         tags_hash.merge! processor.process(tags_hash)
       end
     end
 
     protected
 
-    def create_processors
-      @processors = []
-      @processors << @tags_processor_factory.create_url_remover if @options[:removeurls]
-      @processors << @tags_processor_factory.create_tag_cleaner if @options[:cleantags]
-      @processors << @tags_processor_factory.create_tag_normalizer
-      @processors << @tags_processor_factory.create_tag_setter(@options)
+    def create_processor_chain
+      chain = []
+      chain << @tags_processor_factory.create_url_remover if @options[:removeurls]
+      chain << @tags_processor_factory.create_tag_cleaner if @options[:cleantags]
+      chain << @tags_processor_factory.create_tag_normalizer
+      chain << @tags_processor_factory.create_tag_setter(@options)
+      chain
     end
 
   end
