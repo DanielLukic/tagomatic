88fbc94a3a6ed086b8d77c26edd3b58a26ba0ab7
Daniel Lukic <daniel.lukic@berlinfactor.com>
2009-11-04 21:53:41 +0100

    Switched UrlRemover to work case-insensitive.

--- lib/tagomatic/url_remover.rb
+++ lib/tagomatic/url_remover.rb
@@ -6,10 +6,12 @@ module Tagomatic
       result = {}
       tags_hash.each do |tag,value|
         next unless value
-        result[tag] = value.gsub(/www\.[^\.]+\.[a-z]{2,4}/, '').gsub(/ by [a-zA-Z0-9]+[\w ]/, '')
+        result[tag] = value.gsub(URL_REGEXP, '').gsub(/ by [a-zA-Z0-9]+[\w ]/, '')
       end
       result
     end
+
+    URL_REGEXP = Regexp.compile("www\.[^\.]+\.[a-z]{2,4}", Regexp::IGNORECASE)
 
   end
 
