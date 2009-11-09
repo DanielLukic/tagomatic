d042975f265f0c1bfd69f82ec561712821eca4a6
Daniel Lukic <daniel.lukic@berlinfactor.com>
2009-11-09 16:04:23 +0100

    Introduced LocalOptions class for handling processing of local option file contents (.tagomatic).

---
+++ lib/tagomatic/local_options.rb
@@ -0,0 +1,45 @@
+module Tagomatic
+
+  class LocalOptions
+
+    LOCAL_OPTIONS_FILE_NAME = '.tagomatic'
+
+    def initialize(options, options_parser, options_matcher, logger)
+      @options = options
+      @options_parser = options_parser
+      @options_matcher = options_matcher
+      @logger = logger
+      @options_stack = []
+    end
+
+    def create_child_context
+      cloned = @options.clone
+      cloned[:formats] = @options[:formats].clone
+      @options_stack << cloned
+    end
+
+    def apply_local_options(option_lines)
+      local_options = process_option_lines(option_lines)
+      @logger.verbose "applying local options: #{local_options}"
+      @options_parser.parse!(local_options)
+    end
+
+    def pop_child_context
+      previous_options = @options_stack.pop
+      @options.replace(previous_options)
+    end
+
+    protected
+
+    def process_option_lines(option_lines)
+      local_options = []
+      option_lines.each do |line|
+        @options_matcher.process!(line)
+        local_options.concat @options_matcher.to_argv
+      end
+      local_options
+    end
+
+  end
+
+end
