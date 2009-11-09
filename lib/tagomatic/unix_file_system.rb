c7c642ab0314ea74a96c2f400158780ff9b3f259
Daniel Lukic <daniel.lukic@berlinfactor.com>
2009-11-09 15:06:21 +0100

    Added UnixFileSystem class as initial file system abstraction for Scanner class.

---
+++ lib/tagomatic/unix_file_system.rb
@@ -0,0 +1,39 @@
+module Tagomatic
+
+  class UnixFileSystem
+
+    def expand_path(file_path)
+      File.expand_path(file_path)
+    end
+
+    def join_path(*path_parts)
+      File.join(path_parts)
+    end
+
+    def exist?(file_path)
+      File.exist?(file_path)
+    end
+
+    def is_directory?(file_path)
+      File.directory?(file_path)
+    end
+
+    def is_file?(file_path)
+      File.file?(file_path)
+    end
+
+    def extract_extension(file_path)
+      File.extname(file_path)
+    end
+
+    def read_lines_without_linefeed(file_path)
+      File.readlines(file_path).map {|line| line.chomp}
+    end
+
+    def list_folder_sorted(folder_path)
+      Dir.entries(folder_path).sort
+    end
+
+  end
+
+end
