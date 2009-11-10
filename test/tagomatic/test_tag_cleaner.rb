0398013d1b1b4de81db9ab1fc540ee7d812dacd1
Daniel Lukic <daniel.lukic@berlinfactor.com>
2009-11-10 14:34:15 +0100

    Fixed TagCleaner test case to use Tags module constants now.

--- test/tagomatic/test_tag_cleaner.rb
+++ test/tagomatic/test_tag_cleaner.rb
@@ -1,23 +1,25 @@
 require 'helper'
 
 require 'tagomatic/tag_cleaner'
+require 'tagomatic/tags'
 
 class TestTagCleaner < Test::Unit::TestCase
+  include Tagomatic::Tags
   context "A TagCleaner" do
     setup do
       @tag_cleaner = Tagomatic::TagCleaner.new
-      @test_tags = {'a' => 'artist', 'b' => 'artist - album', 't' => 'artist - album - 01 - title'}
+      @test_tags = {ARTIST => 'artist', ALBUM => 'artist - album', TITLE => 'artist - album - 01 - title'}
     end
 
     should "remove artist name from album name" do
       result = @tag_cleaner.process(@test_tags)
-      assert_equal 'artist', result['a']
-      assert_equal 'album', result['b']
+      assert_equal 'artist', result[ARTIST]
+      assert_equal 'album', result[ALBUM]
     end
 
     should "remove artist and album name from title" do
       result = @tag_cleaner.process(@test_tags)
-      assert_equal '01 - title', result['t']
+      assert_equal '01 - title', result[TITLE]
     end
   end
 end
