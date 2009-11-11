4355032758c6a6447264ff1c6f221e64e454efa8
Daniel Lukic <daniel.lukic@berlinfactor.com>
2009-11-11 09:44:33 +0100

    Removed the example tmp_spec.rb file.

--- spec/tmp_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
-describe "Tagomatic" do
-  it "fails" do
-    pending 'implement something'
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
-end
