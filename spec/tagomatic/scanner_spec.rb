13da907aafa3042f2b4dbb68965dc6c436507731
Daniel Lukic <daniel.lukic@berlinfactor.com>
2009-11-11 09:45:03 +0100

    Added a basic Scanner spec: Checking single file and folder scanning.

---
+++ spec/tagomatic/scanner_spec.rb
@@ -0,0 +1,74 @@
+require 'tagomatic/local_options'
+require 'tagomatic/logger'
+require 'tagomatic/scanner'
+require 'tagomatic/unix_file_system'
+
+describe "Scanner" do
+
+  before do
+    @options = {}
+    @file_system = Tagomatic::UnixFileSystem.new
+    @logger = mock(Tagomatic::Logger, :verbose => nil)
+    @local_options = mock(Tagomatic::LocalOptions, :create_child_context => nil, :pop_child_context => nil)
+    @scanner = Tagomatic::Scanner.new(@options, @file_system, @local_options, @logger)
+    @result = []
+  end
+
+  shared_examples_for 'scanning single input file' do
+
+    it "yields single file if this is the only input" do
+      @scanner.each_mp3('test/data/scanner_spec/test1.mp3') { |f| collect f }
+      @result.sort.should == ['test1.mp3']
+    end
+
+  end
+
+  describe 'scanning with :recurse => false' do
+
+    before do
+      @options[:recurse] = false
+    end
+
+    it "yields nothing if scanning folder" do
+      @scanner.each_mp3(TEST_DATA_FOLDER) { |f| collect f }
+      @result.sort.should == []
+    end
+
+    it_should_behave_like 'scanning single input file'
+
+  end
+
+  describe 'scanning with :recurse => true' do
+
+    before do
+      @options[:recurse] = true
+    end
+
+    it "yields all taggable files when scanning with :recurse option on" do
+      @scanner.each_mp3(TEST_DATA_FOLDER) { |f| collect f }
+      @result.sort.should == ['cheese.mp3', 'test1.mp3', 'test2.mp3', 'toast.mp3']
+    end
+
+    it "yields absolute file pathes" do
+      @scanner.each_mp3(TEST_DATA_FOLDER) do |mp3_file_path|
+         has_absolute_path?(mp3_file_path).should == true
+      end
+    end
+
+    it_should_behave_like 'scanning single input file'
+
+  end
+
+  def collect(mp3_file_path)
+    @result ||= []
+    @result << File.basename(mp3_file_path)
+  end
+
+  def has_absolute_path?(mp3_file_path)
+    path_prefix = File.expand_path(TEST_DATA_FOLDER)
+    mp3_file_path.starts_with?(path_prefix)
+  end
+
+  TEST_DATA_FOLDER = 'test/data/scanner_spec'
+
+end
